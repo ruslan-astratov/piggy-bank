00d6cce786806789301a8f1a1268949c8b1a9a65
Yekaterina S <you@example.com>
2021-04-07 16:05:50 +0500

    Added function for calculating months

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ let percent = document.querySelector('#percent');
 let monthlyPayment = document.querySelector('#monthly-payment');
 let formSaveBtn = document.querySelector('#form-save-btn');
 let formCancelBtn = document.querySelector('#form-cancel-btn');
+
+// Написать функцию  которая возвращает количество месяцев от сегодняшнего дня до введенной даты. 
+// Ограничить ввод даты из прошлого
+
+function calcMonths () {
+
+    let start =  moment().format('YYYY-MM')
+    let end = period.innerHTML;
+    let a = moment(start, "YYYY-MM");
+    let b = moment(end, "YYYY-MM");
+    if (b.isBefore(a.startOf('month'))) {
+        return 'Invalid Date'
+    }
+    let  months = b.diff(a, 'month') ;
+    return months;
+}
